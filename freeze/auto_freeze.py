5512a3aa89927cfa9579ae15ce92d51200189a19
Chaoyang He <chaoyanghe.com@gmail.com>
2020-12-27 12:48:10 -0800

    Update auto_freeze.py

--- freeze/auto_freeze.py
+++ freeze/auto_freeze.py
@@ -16,12 +16,12 @@ class AutoFreeze:
         num_freeze_layers = 0
         if epoch == 0:
             num_freeze_layers = 0
-        elif epoch > 1 and epoch <= 3:
-            num_freeze_layers = epoch
+        elif epoch > 1 and epoch <= 2:
+            num_freeze_layers = 4
         elif epoch > 3 and epoch <= 5:
-            num_freeze_layers = 5
+            num_freeze_layers = 8
         elif epoch > 5 and epoch <= 7:
-            num_freeze_layers = 7
+            num_freeze_layers = 10
         elif epoch > 7:
             num_freeze_layers = 12
         return num_freeze_layers
