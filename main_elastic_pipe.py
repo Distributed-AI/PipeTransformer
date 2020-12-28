519534789ffd5190afb8479e75f07265b48aa7a7
Chaoyang He <chaoyanghe.com@gmail.com>
2020-12-27 16:29:30 -0800

    Update main_elastic_pipe.py

--- main_elastic_pipe.py
+++ main_elastic_pipe.py
@@ -184,6 +184,7 @@ def train(args, auto_pipe, auto_dp, model, epoch, train_dataloader, test_dataloa
         # recv_gbyte, transmit_gbyte = net_meter.update_bandwidth()
         # logging.info("BW {recv_MB:%.3f} {transmit_MB:%.3f}" % (recv_gbyte * 1024, transmit_gbyte * 1024))
 
+        logging.info("---------------------time cost:")
         sync_all_devices(0, auto_pipe.get_pipe_len())
         if batch_idx == 0:
             time_finish_prepare_ddp = time.time()
@@ -196,6 +197,7 @@ def train(args, auto_pipe, auto_dp, model, epoch, train_dataloader, test_dataloa
         # with torch.cuda.device(device_last):
         logging.info(f"backwards time cost: (s) by CUDA event {start_bp.elapsed_time(end_bp)/1000}")
 
+        logging.info("-------------------------------------")
         sample_num_throughput = int(
             num_sample_processed_in_total / (time.time() - time_finish_prepare_ddp)) * auto_dp.get_active_world_size()
         logging.info("global_rank = %d. sample_num_throughput (images/second): %d" % (auto_dp.get_global_rank(),
