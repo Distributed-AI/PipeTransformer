4a8dc562417d58024a84cbbeee7aa421224cc7e8
Chaoyang He <chaoyanghe.com@gmail.com>
2020-12-30 14:25:44 -0800

    debug

--- run_cifar100_batch_experiment_node0.sh
+++ run_cifar100_batch_experiment_node0.sh
@@ -1,11 +1,17 @@
+# nohup sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.01 320 cifar100 ./data/cifar100/ 8 1 ./data/cifar100/ 8 1 > ./PipeTransformer-CIFAR100-node0.log 2>&1 &
 sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.03 320 cifar100 ./data/cifar100/ 8 1
 
+# nohup sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.01 320 cifar100 ./data/cifar100/ 8 1 ./data/cifar100/ 8 1 > ./PipeTransformer-CIFAR100-node0.log 2>&1 &
 sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.01 320 cifar100 ./data/cifar100/ 8 1
 
+# nohup sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.3 320 cifar100 ./data/cifar100/ 8 1 ./data/cifar100/ 8 1 > ./PipeTransformer-CIFAR100-node0.log 2>&1 &
 sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.3 320 cifar100 ./data/cifar100/ 8 1
 
+# nohup sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.1 320 cifar100 ./data/cifar100/ 8 1 ./data/cifar100/ 8 1 > ./PipeTransformer-CIFAR100-node0.log 2>&1 &
 sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.1 320 cifar100 ./data/cifar100/ 8 1
 
+# nohup sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.03 320 cifar100 ./data/cifar100/ 8 1 ./data/cifar100/ 8 1 > ./PipeTransformer-CIFAR100-node0.log 2>&1 &
 sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.03 320 cifar100 ./data/cifar100/ 8 1
 
+# nohup sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.03 320 cifar100 ./data/cifar100/ 8 1 ./data/cifar100/ 8 1 > ./PipeTransformer-CIFAR100-node0.log 2>&1 &
 sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.03 320 cifar100 ./data/cifar100/ 8 1
