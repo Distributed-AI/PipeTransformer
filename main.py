6e75facfc4e4903c5d1734093dc4d708f255ad7a
Chaoyang He <chaoyanghe.com@gmail.com>
2020-12-30 01:12:19 -0800

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s.%(msecs)03d %(levelname)s {%(module)s} [%(funcName)s] %(message)s',
                         datefmt='%Y-%m-%d,%H:%M:%S')
-    logging.getLogger().setLevel(logging.INFO)
+    logging.getLogger(__name__).setLevel(logging.INFO)
 
     hostname = socket.gethostname()
     logging.info("#############process ID = " +
