afcd4950d691c2f0a6f9baff3fa13e366bdd35eb
chaoyanghe <me@chaoyanghe.com>
2021-01-02 19:16:44 -0800

    debug

--- run_imagenet_batch_experiment_node0.sh
+++ run_imagenet_batch_experiment_node0.sh
@@ -1,17 +1,17 @@
 
 
 
-# nohup sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.03 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1 > ./PipeTransformer-imagenet-node1.log 2>&1 &
-sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.03 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1
+# nohup sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 "ib0" 0.03 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1 > ./PipeTransformer-imagenet-node1.log 2>&1 &
+sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 "ib0" 0.03 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1
 
-sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.01 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1
+sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 "ib0" 0.01 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1
 
-sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.3 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1
+sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 "ib0" 0.3 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1
 
 # nohup sh
-# nohup sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.1 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1 > ./PipeTransformer-imagenet-node0.log 2>&1 &
-sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.1 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1
+# nohup sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 "ib0" 1 0.1 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1 > ./PipeTransformer-imagenet-node0.log 2>&1 &
+sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 "ib0" 0.1 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1
 
-sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.003 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1
+sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 "ib0" 0.003 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1
 
-sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 0.001 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1
+sh run_elastic_pipe.sh 8 2 0 192.168.11.2 22222 1 "ib0" 0.001 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1
