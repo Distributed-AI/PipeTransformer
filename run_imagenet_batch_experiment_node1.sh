baecabcbd36fd0ea8d0d362af95ba06391c928ca
Chaoyang He <chaoyanghe.com@gmail.com>
2021-01-04 02:12:26 -0800

    debug

--- run_imagenet_batch_experiment_node1.sh
+++ run_imagenet_batch_experiment_node1.sh
@@ -1,7 +1,8 @@
 
+sh run_elastic_pipe.sh 8 2 1 192.168.11.2 22222 1 "ib0" 0.01 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1
+
 sh run_elastic_pipe.sh 8 2 1 192.168.11.2 22222 1 "ib0" 0.3 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1
 
-sh run_elastic_pipe.sh 8 2 1 192.168.11.2 22222 1 "ib0" 0.01 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1
 
 # nohup sh run_elastic_pipe.sh 8 2 1 192.168.11.2 22222 1 "ib0" 0.03 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1 > ./PipeTransformer-CIFAR100-node1.log 2>&1 &
 sh run_elastic_pipe.sh 8 2 1 192.168.11.2 22222 1 "ib0" 0.03 400 imagenet /home/chaoyanghe/dataset/cv/imagenet 8 1
