3f155f8e2061af2e25b6e469ce054b0148bcf9dd
Chaoyang He <chaoyanghe.com@gmail.com>
2021-01-12 18:15:08 -0800

    Update auto_cache_impl.py

--- cache/auto_cache_impl.py
+++ cache/auto_cache_impl.py
@@ -110,6 +110,7 @@ class AutoCacheImpl:
         while not self.msg_q.empty():
             self.msg_q.get()
         self.msg_q.close()
+        self.cache_daemon.join()
         self.cache_daemon.terminate()
         self.cache_daemon.kill()
 
