b125883b3e50a1977a8dc8319f3faa91bd06d95a
Chaoyang He <chaoyanghe.com@gmail.com>
2021-01-14 03:26:50 -0800

    Update main.py

--- main.py
+++ main.py
@@ -85,8 +85,9 @@ if __name__ == "__main__":
     parser.add_argument("--is_infiniband", default=1, type=int,
                         help="is_infiniband")
 
-    # pipelin = 2
-    parser.add_argument("--num_chunks_of_micro_batches", default=8, type=int,
+    # pipelin = 2 * 4
+    # pipelin = 2 * 2
+    parser.add_argument("--num_chunks_of_micro_batches", default=4, type=int,
                         help="num_chunks_of_micro_batches")
 
     parser.add_argument('--freeze', dest='b_freeze', action='store_true')
